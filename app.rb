9709f7bb8fa3d0beb8e489222a7dea8c9b012ea9
spenser6131 <brinkman.spenser@gmail.com>
2020-11-05 14:58:32 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Spenser Brinkman"
+  end
+  
+  get '/hometown' do
+    "My hometown is Brentwood, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Buckethead - Whitewash"
+  end
 end
